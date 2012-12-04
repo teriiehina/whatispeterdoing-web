18f285b2a904821311ad2ff9127e5b14568485ba
Peter MEUEL <peter@teriiehina.net>
2012-12-04 14:27:03 +0100

    retrieving the last activity from parse.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php 
-  include 'lib/parseConfig.php';
+  include 'lib/parse.php';
+//    $parse       = new parseObject('Activity');
+//    $parse->name = "working";
+//    $r           = $parse->save();
 
-    //This example is a sample video upload stored in parse
-
-    $parse       = new parseObject('Activity');
-    $parse->name = "working";
-    $r           = $parse->save();
+  $query   = new parseQuery('Activity');
+  $query->orderByDescending('createdAt');
+  $results = $query->find();
 ?>
+peter is <?= $results->results[0]->verb ?> <?= $results->results[0]->extra ?>

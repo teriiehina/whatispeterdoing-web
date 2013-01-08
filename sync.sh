8d4cc966d5c5a26253e0ca75403f620fc209fb2b
Peter MEUEL <peter@teriiehina.net>
2013-01-08 14:22:11 +0100

    use rsync instead of rcp

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
-scp -r index.php test.html img/ css/ whatisadmin@whatispeterdoing.com:whatispeterdoing.com/
+#scp -r index.php lib/ img/ css/ whatisadmin@whatispeterdoing.com:whatispeterdoing.com/
+rsync -avz -e ssh whatisadmin@whatispeterdoing.com:whatispeterdoing.com ./ 

9b8312df176aa278edaf367ae3d8d2bbb7153508
Peter MEUEL <peter@teriiehina.net>
2013-01-08 14:07:31 +0100

    fix the sync of the 'img' folder

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-scp -r index.php test.html images/ css/ whatisadmin@whatispeterdoing.com:whatispeterdoing.com/
+scp -r index.php test.html img/ css/ whatisadmin@whatispeterdoing.com:whatispeterdoing.com/
